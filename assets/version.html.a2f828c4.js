713cae452b1ef09976f319a6ffbea797a0871f9a
hustcer <hustcer@users.noreply.github.com>
2022-06-03 06:25:33 +0000

    deploy: 73cb0e1d672065c688964169447ba272e64b380b

--- assets/version.html.a2f828c4.js
+++
@@ -1,5 +0,0 @@
-import{_ as e,o as s,c as a,e as n}from"./app.aa868a80.js";const t={},r=n(`<p>Outputs the nushell version.</p><h2 id="examples" tabindex="-1"><a class="header-anchor" href="#examples" aria-hidden="true">#</a> Examples</h2><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> version
-\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
- version \u2502 <span class="token number">0.15</span>.1
-\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
-</code></pre></div>`,3),o=[r];function c(l,i){return s(),a("div",null,o)}var d=e(t,[["render",c],["__file","version.html.vue"]]);export{d as default};
