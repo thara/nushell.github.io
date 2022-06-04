7817464e2785afdbca1cdf1103d5cf30c8d5d770
hustcer <hustcer@users.noreply.github.com>
2022-06-04 10:03:27 +0000

    deploy: 97dc084e746104e789ee777361d7a633bc6f9dea

---
+++ assets/date.html.5209ad1b.js
@@ -0,0 +1,36 @@
+import{_ as n,o as a,c as e,e as s}from"./app.c480cfbf.js";const t={},o=s(`<p>Use <code>date</code> to get the current date and time. Defaults to local timezone but you can get it in UTC too.</p><h2 id="flags" tabindex="-1"><a class="header-anchor" href="#flags" aria-hidden="true">#</a> Flags</h2><pre><code>--utc
+  Returns the current date and time in UTC
+
+--local
+  Returns the current date and time in your local timezone
+</code></pre><h2 id="examples" tabindex="-1"><a class="header-anchor" href="#examples" aria-hidden="true">#</a> Examples</h2><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token function">date</span>
+\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+ year     \u2502 <span class="token number">2020</span>
+ month    \u2502 <span class="token number">6</span>
+ day      \u2502 <span class="token number">21</span>
+ hour     \u2502 <span class="token number">18</span>
+ minute   \u2502 <span class="token number">3</span>
+ second   \u2502 <span class="token number">43</span>
+ timezone \u2502 -04:00
+\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+</code></pre></div><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token function">date</span> --utc
+\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u2500\u2500\u2500
+ year     \u2502 <span class="token number">2020</span>
+ month    \u2502 <span class="token number">6</span>
+ day      \u2502 <span class="token number">21</span>
+ hour     \u2502 <span class="token number">22</span>
+ minute   \u2502 <span class="token number">3</span>
+ second   \u2502 <span class="token number">53</span>
+ timezone \u2502 UTC
+\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500
+</code></pre></div><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token function">date</span> --local
+\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+ year     \u2502 <span class="token number">2020</span>
+ month    \u2502 <span class="token number">6</span>
+ day      \u2502 <span class="token number">21</span>
+ hour     \u2502 <span class="token number">18</span>
+ minute   \u2502 <span class="token number">4</span>
+ second   \u2502 <span class="token number">3</span>
+ timezone \u2502 -04:00
+\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+</code></pre></div>`,7),c=[o];function r(p,l){return a(),e("div",null,c)}var u=n(t,[["render",r],["__file","date.html.vue"]]);export{u as default};
---
+++ assets/from-url.html.0cf24550.js
@@ -0,0 +1,8 @@
+import{_ as n,r as s,o,c as r,a,b as c,d as e,e as l}from"./app.c480cfbf.js";const p={},d=e("Parse "),h={href:"https://url.spec.whatwg.org/#application/x-www-form-urlencoded",target:"_blank",rel:"noopener noreferrer"},i=e("url-encoded string"),m=e(" as a table."),_=l(`<h2 id="example" tabindex="-1"><a class="header-anchor" href="#example" aria-hidden="true">#</a> Example</h2><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token builtin class-name">echo</span> <span class="token string">&#39;bread=baguette&amp;cheese=comt%C3%A9&amp;meat=ham&amp;fat=butter&#39;</span> <span class="token operator">|</span> from url
+\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+ bread  \u2502 baguette
+ cheese \u2502 comt\xE9
+ meat   \u2502 ham
+ fat    \u2502 butter
+\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+</code></pre></div>`,2);function u(f,b){const t=s("ExternalLinkIcon");return o(),r("div",null,[a("p",null,[d,a("a",h,[i,c(t)]),m]),_])}var x=n(p,[["render",u],["__file","from-url.html.vue"]]);export{x as default};
