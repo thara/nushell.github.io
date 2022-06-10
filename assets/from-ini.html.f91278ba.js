5fb06c262a2a7d8af1d241dc6816fbb359189daf
hustcer <hustcer@users.noreply.github.com>
2022-06-10 07:14:07 +0000

    deploy: 55f7294f4c53bc8e7ec58d6678b2a8bf822fa016

---
+++ assets/from-ini.html.f91278ba.js
@@ -0,0 +1,13 @@
+import{_ as e,o as n,c as a,e as s}from"./app.420e319b.js";const t={},o=s(`<p>Converts ini data into table. Use this when nushell cannot determine the input file extension.</p><h2 id="example" tabindex="-1"><a class="header-anchor" href="#example" aria-hidden="true">#</a> Example</h2><p>Let&#39;s say we have the following <code>.txt</code> file:</p><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token function">open</span> sample.txt
+<span class="token punctuation">[</span>SectionOne<span class="token punctuation">]</span>
+
+key <span class="token operator">=</span> value
+integer <span class="token operator">=</span> <span class="token number">1234</span>
+string1 <span class="token operator">=</span> <span class="token string">&#39;Case 1&#39;</span>
+</code></pre></div><p>This file is actually a ini file, but the file extension isn&#39;t <code>.ini</code>. That&#39;s okay, we can use the <code>from ini</code> command:</p><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token function">open</span> sample.txt <span class="token operator">|</span> from ini <span class="token operator">|</span> get SectionOne
+\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501
+ key   \u2502 integer \u2502 string1
+\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+ value \u2502 <span class="token number">1234</span>    \u2502 <span class="token string">&#39;Case 1&#39;</span>
+\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501
+</code></pre></div>`,6),p=[o];function c(i,l){return n(),a("div",null,p)}var d=e(t,[["render",c],["__file","from-ini.html.vue"]]);export{d as default};
---
+++ assets/prepend.html.9ea9678c.js
@@ -0,0 +1,34 @@
+import{_ as n,o as s,c as a,e}from"./app.420e319b.js";const t={},o=e(`<p>This command prepends the given row to the front of the table</p><p><strong>Note</strong>:</p><ul><li><code>prepend</code> does not change a file itself. If you want to save your changes, you need to run the <code>save</code> command</li><li>if you want to add something containing a whitespace character, you need to put it in quotation marks</li></ul><h2 id="examples" tabindex="-1"><a class="header-anchor" href="#examples" aria-hidden="true">#</a> Examples</h2><p>Let&#39;s complete this table with the missing continents:</p><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token function">open</span> continents.txt <span class="token operator">|</span> lines
+\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501
+ <span class="token comment"># \u2502</span>
+\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+ <span class="token number">0</span> \u2502 Africa
+ <span class="token number">1</span> \u2502 South America
+ <span class="token number">2</span> \u2502 Australia
+ <span class="token number">3</span> \u2502 Europe
+ <span class="token number">4</span> \u2502 Antarctica
+\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501
+</code></pre></div><p>You can add a new row at the top by using <code>prepend</code>:</p><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token function">open</span> continents.txt <span class="token operator">|</span> lines <span class="token operator">|</span> prepend Asia
+\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501
+ <span class="token comment"># \u2502</span>
+\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+ <span class="token number">0</span> \u2502 Asia
+ <span class="token number">1</span> \u2502 Africa
+ <span class="token number">2</span> \u2502 South America
+ <span class="token number">3</span> \u2502 Australia
+ <span class="token number">4</span> \u2502 Europe
+ <span class="token number">5</span> \u2502 Antarctica
+\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501
+</code></pre></div><p>It&#39;s not possible to add multiple rows at once, so you&#39;ll need to call <code>prepend</code> multiple times:</p><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token function">open</span> continents.txt <span class="token operator">|</span> lines <span class="token operator">|</span> prepend Asia <span class="token operator">|</span> prepend <span class="token string">&quot;North America&quot;</span>
+\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501
+ <span class="token comment"># \u2502</span>
+\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+ <span class="token number">0</span> \u2502 North America
+ <span class="token number">1</span> \u2502 Asia
+ <span class="token number">2</span> \u2502 Africa
+ <span class="token number">3</span> \u2502 South America
+ <span class="token number">4</span> \u2502 Australia
+ <span class="token number">5</span> \u2502 Europe
+ <span class="token number">6</span> \u2502 Antarctica
+\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501
+</code></pre></div>`,10),p=[o];function c(r,l){return s(),a("div",null,p)}var u=n(t,[["render",c],["__file","prepend.html.vue"]]);export{u as default};
