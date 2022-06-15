2ce9d46dc84759aedd04c141d0f45a5170657f6e
hustcer <hustcer@users.noreply.github.com>
2022-06-15 04:59:29 +0000

    deploy: aae9b6a9a38aec943370fd38f488ba702f135d7f

---
+++ assets/echo.html.701cba78.js
@@ -0,0 +1,10 @@
+import{_ as e,o as a,c as s,e as n}from"./app.94815f9c.js";const o={},t=n(`<p>Use <code>echo</code> to repeat arguments back to the user</p><h2 id="examples" tabindex="-1"><a class="header-anchor" href="#examples" aria-hidden="true">#</a> Examples</h2><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token builtin class-name">echo</span> Hello world
+\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+ <span class="token comment"># \u2502</span>
+\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+ <span class="token number">0</span> \u2502 Hello
+ <span class="token number">1</span> \u2502 world
+\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500
+</code></pre></div><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token builtin class-name">echo</span> <span class="token string">&quot;Hello, world!&quot;</span>
+Hello, world<span class="token operator">!</span>
+</code></pre></div>`,4),c=[t];function l(r,p){return a(),s("div",null,c)}var h=e(o,[["render",l],["__file","echo.html.vue"]]);export{h as default};
---
+++ assets/shuffle.html.cabba0ac.js
@@ -0,0 +1,22 @@
+import{_ as s,o as n,c as a,e}from"./app.94815f9c.js";const p={},t=e(`<p>Shuffles the rows in a random order.</p><h2 id="examples" tabindex="-1"><a class="header-anchor" href="#examples" aria-hidden="true">#</a> Examples</h2><p>Passing the same input to shuffle multiple times gives different results -</p><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token builtin class-name">echo</span> <span class="token punctuation">[</span> a b c d <span class="token punctuation">]</span> <span class="token operator">|</span> shuffle
+\u2500\u2500\u2500\u252C\u2500\u2500\u2500
+ <span class="token number">0</span> \u2502 a
+ <span class="token number">1</span> \u2502 c
+ <span class="token number">2</span> \u2502 d
+ <span class="token number">3</span> \u2502 b
+\u2500\u2500\u2500\u2534\u2500\u2500\u2500
+</code></pre></div><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token builtin class-name">echo</span> <span class="token punctuation">[</span> a b c d <span class="token punctuation">]</span> <span class="token operator">|</span> shuffle
+\u2500\u2500\u2500\u252C\u2500\u2500\u2500
+ <span class="token number">0</span> \u2502 c
+ <span class="token number">1</span> \u2502 b
+ <span class="token number">2</span> \u2502 d
+ <span class="token number">3</span> \u2502 a
+\u2500\u2500\u2500\u2534\u2500\u2500\u2500
+</code></pre></div><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token builtin class-name">echo</span> <span class="token punctuation">[</span> a b c d <span class="token punctuation">]</span> <span class="token operator">|</span> shuffle
+\u2500\u2500\u2500\u252C\u2500\u2500\u2500
+ <span class="token number">0</span> \u2502 c
+ <span class="token number">1</span> \u2502 b
+ <span class="token number">2</span> \u2502 a
+ <span class="token number">3</span> \u2502 d
+\u2500\u2500\u2500\u2534\u2500\u2500\u2500
+</code></pre></div>`,6),c=[t];function o(l,r){return n(),a("div",null,c)}var i=s(p,[["render",o],["__file","shuffle.html.vue"]]);export{i as default};
