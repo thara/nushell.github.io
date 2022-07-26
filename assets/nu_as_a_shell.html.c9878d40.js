3d58e64462fbf1e5c8ae99afdd576fe6b93da95c
kubouch <kubouch@users.noreply.github.com>
2022-07-26 19:46:12 +0000

    deploy: b024f6d6ad907c48d12f309cdc7ebad12352ea90

---
+++ assets/nu_as_a_shell.html.c9878d40.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-2d2ccd89","path":"/book/nu_as_a_shell.html","title":"Nu as a Shell","lang":"English","frontmatter":{},"excerpt":"","headers":[],"filePathRelative":"book/nu_as_a_shell.md","git":{"createdTime":1658864458000,"updatedTime":1658864458000,"contributors":[{"name":"Jakub \u017D\xE1dn\xEDk","email":"kubouch@gmail.com","commits":1}]}}');export{e as data};
