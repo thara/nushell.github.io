d551d9427352033e130be17bc98d02790c6b79de
sholderbach <sholderbach@users.noreply.github.com>
2022-11-02 22:41:22 +0000

    deploy: ec3850f45d3a7de607923ef1cc7a83d646f1ccb4

---
+++ assets/from_eml.html.351e80a3.js
@@ -0,0 +1,11 @@
+import{_ as o,o as r,c as n,a as e,t,d as s,e as c}from"./app.8af834da.js";const l={},d={id:"frontmatter-title-for-formats",tabindex:"-1"},i=e("a",{class:"header-anchor",href:"#frontmatter-title-for-formats","aria-hidden":"true"},"#",-1),m=s(),p=s(" for formats"),h={class:"command-title"},f=c(`<h2 id="signature" tabindex="-1"><a class="header-anchor" href="#signature" aria-hidden="true">#</a> Signature</h2><p><code>&gt; from eml --preview-body</code></p><h2 id="parameters" tabindex="-1"><a class="header-anchor" href="#parameters" aria-hidden="true">#</a> Parameters</h2><ul><li><code>--preview-body {int}</code>: How many bytes of the body to preview</li></ul><h2 id="examples" tabindex="-1"><a class="header-anchor" href="#examples" aria-hidden="true">#</a> Examples</h2><p>Convert eml structured data into table</p><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token string">&#39;From: test@email.com
+Subject: Welcome
+To: someone@somewhere.com
+
+Test&#39;</span> <span class="token operator">|</span> from eml
+</code></pre></div><p>Convert eml structured data into table</p><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token operator">&gt;</span> <span class="token string">&#39;From: test@email.com
+Subject: Welcome
+To: someone@somewhere.com
+
+Test&#39;</span> <span class="token operator">|</span> from eml <span class="token parameter variable">-b</span> <span class="token number">1</span>
+</code></pre></div>`,9);function u(a,_){return r(),n("div",null,[e("h1",d,[i,m,e("code",null,t(a.$frontmatter.title),1),p]),e("div",h,t(a.$frontmatter.formats),1),f])}const g=o(l,[["render",u],["__file","from_eml.html.vue"]]);export{g as default};
